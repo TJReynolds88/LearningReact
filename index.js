67bbb07bff447ab4f7a04f65797b12d42c80d990
TJReynolds88 <tj.reynolds1988@gmail.com>
2019-03-25 09:36:31 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,63 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-let runData = {
-    total: 50,
-    rainy: 20,
-    track: 10,
-    goal: 100
-}
-const getPercent = decimal  => {
-    return decimal * 100 + '%'
-}
 
-const calcGoalProgress = (total,goal) => {
-    return getPercent(total/goal)
-}
+let bookList = [
+    {"title": "The Sun Rises", "author": "Ernest Hemingway", "pages:":260},
+    {"title": "White Teeth", "author": "Zadie Smith", "pages:":480},
+    {"title": "Cat's Craddle", "author": "Kurt Vonnehut", "pages:":304}
+]
 
 
-const RunDayCounter = ({total, rainy, track, goal}) => {
-    return(
+const Book = ({title, author, pages}) => {
+    return (
         <section>
-        <div>
-        <p>Total Days: {total}</p>
-        </div>
-        <div>
-        <p>Rainy Days: {rainy}</p>
-        </div>
-        <div>
-        <p>Track Days: {track}</p>
-        </div>
-        <div>
-        <p>Goal Progress: {calcGoalProgress( total, goal)}</p>
-        </div>
-    </section>
-        
+            <h2>{title}</h2>
+            <p>by: {author}</p>
+            <p>Pages: {pages} pages</p>
+        </section>
     )
+
+
+}
+
+
+
+class Library extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            open: open
+        }
+
+    }
+    render (){
+        const {books} = this.props
+        return (
+           
+            <div>
+                 <h1>The Library is {this.state.open ? 'open' : 'closed'}</h1>
+                {books.map(
+                    (book, i) =>
+                    <Book 
+                    key={i}
+                    title={book.title} 
+                    author={book.author} 
+                    pages={book.pages}/>
+                )}
+            </div>
+        )
+    }
+
 }
 
 
 
 
-render(<RunDayCounter
-                total={runData.total}
-                rainy={runData.rainy}
-                track={runData.track}
-                goal={runData.goal} />, document.getElementById('root')
+
+render(
+    
+    <Library books={bookList}/>
+            , document.getElementById('root')
 )
 
